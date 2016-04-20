0f013edb3380717a46ecdd1a083c02d88c364183
Zhuanhao Wu <allen_ng@foxmail.com>
2016-04-21 07:01:04 +0800

    Add SAM

--- string.h
+++ string.h
@@ -4,7 +4,7 @@
 
 namespace acm {
 	namespace str {
-		#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 		using namespace std;
 		const int maxn = 1e5;
 
@@ -22,7 +22,7 @@ namespace acm {
 		int LCP[maxn]; // height/Logest Common Prefix, from 1
 		int stp; // used to access P
 
-		
+
 		// O(n) build lcp
 		void build_lcp(int stp, string& s) {
 			int i, j, k = 0;
@@ -84,82 +84,165 @@ namespace acm {
 		}// namespace trie
 
 		namespace ac_automaton {
-			#include <bits/stdc++.h>
-      using namespace std;
+#include <bits/stdc++.h>
+			using namespace std;
 
-      const int maxn = 10000;
-      struct AC {
-      	map<int, AC*> next;
-      	int c;
-      	int cnt = 0;
-      	AC* fail = NULL, *fa = NULL;
-      } nodes[maxn];
-      int p = 1;
-      AC* root;
+			const int maxn = 10000;
+			struct AC {
+				map<int, AC*> next;
+				int c;
+				int cnt = 0;
+				AC* fail = NULL, *fa = NULL;
+			} nodes[maxn];
+			int p = 1;
+			AC* root;
 
-      void init_ac() {
-      	root = nodes;
-      	root->c = 0;
-      	root->fail = NULL;
-      	root->fa = NULL;
-      }
+			void init_ac() {
+				root = nodes;
+				root->c = 0;
+				root->fail = NULL;
+				root->fa = NULL;
+			}
 
-      void insert(string& s) {
-      	AC* curr = root;
-      	for(auto c : s) {
-      		if(curr->next.find(c) == curr->next.end()) {
-      			curr->next.insert(make_pair(c, &(nodes[p++])));
-      			curr->next[c]->c = c;
-      			curr->next[c]->fa = curr;
-      		}
-      		curr = curr->next[c];
-      	}
-      	curr->cnt++;
-      }
+			void insert(string& s) {
+				AC* curr = root;
+				for(auto c : s) {
+					if(curr->next.find(c) == curr->next.end()) {
+						curr->next.insert(make_pair(c, &(nodes[p++])));
+						curr->next[c]->c = c;
+						curr->next[c]->fa = curr;
+					}
+					curr = curr->next[c];
+				}
+				curr->cnt++;
+			}
 
-      void build_ac() {
-      	static queue<AC*> q;
-      	for(auto pr : root->next) {
-      		q.push(pr.second);
-      	}
-      	AC* curr;
-      	while(!q.empty()) {
-      		AC* rt = q.front();
-      		q.pop();
-      		for(auto pr : rt->next) {
-      			q.push(pr.second);
-      		}
-      		curr = rt->fa == NULL ? NULL : rt->fa->fail;
-      		while(curr != NULL) {
-      			if(curr->next.find(rt->c) == curr->next.end()) {
-      				curr = curr->fail;
-      			} else {
-      				rt->fail = curr->next[rt->c];
-      				break;
-      			}
-      		}
-      		if(curr == NULL) {
-      			rt -> fail = root;
-      		}
-      	}
-      }
+			void build_ac() {
+				static queue<AC*> q;
+				for(auto pr : root->next) {
+					q.push(pr.second);
+				}
+				AC* curr;
+				while(!q.empty()) {
+					AC* rt = q.front();
+					q.pop();
+					for(auto pr : rt->next) {
+						q.push(pr.second);
+					}
+					curr = rt->fa == NULL ? NULL : rt->fa->fail;
+					while(curr != NULL) {
+						if(curr->next.find(rt->c) == curr->next.end()) {
+							curr = curr->fail;
+						} else {
+							rt->fail = curr->next[rt->c];
+							break;
+						}
+					}
+					if(curr == NULL) {
+						rt -> fail = root;
+					}
+				}
+			}
 
-      void query(string& s) {
-      	AC* curr = root;
-      	for(auto c : s) {
-      		while(curr != root && curr->next.find(c) == curr->next.end()) {
-      			curr = curr->fail;
-      		}
-      		if(curr == root && curr->next.find(c) == curr->next.end()) continue;
-      		curr = curr->next[c];
-      		if(curr->cnt != 0) {
-      			cout<<"YES"<<endl;
-      			return;
-      		}
-      	}
-      	cout<<"NO"<<endl;
-      }
-    } // namespace ac_automaton
+			void query(string& s) {
+				AC* curr = root;
+				for(auto c : s) {
+					while(curr != root && curr->next.find(c) == curr->next.end()) {
+						curr = curr->fail;
+					}
+					if(curr == root && curr->next.find(c) == curr->next.end()) continue;
+					curr = curr->next[c];
+					if(curr->cnt != 0) {
+						cout<<"YES"<<endl;
+						return;
+					}
+				}
+				cout<<"NO"<<endl;
+			}
+		} // namespace ac_automaton
+		namespace sam {
+
+			const int maxn = 100100;
+
+			// define maxn beforehand
+			int rt[maxn<<1]; // right
+			int dd[maxn<<1]; // degree
+			// remember change node type !
+			struct node {
+				// 如果被卡next可以考虑换成数组
+				// 0代表无边, 后面克隆节点用Memcpy
+				map<char, int> next;
+				int link, len;
+				int vis = -1;
+			} st[maxn<<1];
+
+
+
+			int sz = 1, last = 0;
+			void init() {
+				st[0].link = -1;
+				st[0].len = 0;
+				sz = 1; last = 0;
+			}
+
+			void debug() {
+				for(int i = 0; i < sz; i++) {
+					cerr<<"[ST "<<i<<"]: "<<endl;
+					for(auto pr : st[i].next) {
+						cerr<<"  ("<<pr.first<<", "<<pr.second<<")"<<endl;
+					}
+					cerr<<"  link: "<<st[i].link<<endl;
+					cerr<<"  len : "<<st[i].len<<endl;
+				}
+			}
+			void sa_extend(char c) {
+				int cur = sz++;
+				rt[cur] = 1;
+				st[cur].len = st[last].len + 1;
+
+				int p;
+				for (p = last; p != -1 && !st[p].next.count(c); p = st[p].link) 
+					st[p].next[c] = cur;
+				if (p == -1)
+					st[cur].link = 0;
+				else {
+					int q = st[p].next[c];
+					if (st[p].len + 1 == st[q].len) {
+						st[cur].link = q;
+					}
+					else {
+						int clone = sz++;
+						st[clone].len = st[p].len + 1;
+						st[clone].next = st[q].next;
+						st[clone].link = st[q].link;
+						for (; p != -1 && st[p].next[c] == q; p = st[p].link) {
+							st[p].next[c] = clone;
+						}
+						st[q].link = st[cur].link = clone;
+
+					}
+				}
+				last = cur;
+			}
+
+			void build_right() {
+				queue<int> q;
+				for (int i = 1; i < sam::sz; i++) dd[sam::st[i].link]++;
+				for (int j = 1; j < sam::sz; j++) if (dd[j] == 0) q.push(j);
+				while (!q.empty()) {
+					int x = q.front(); q.pop();
+					int fa = sam::st[x].link;
+					if (fa != -1) {
+						rt[fa] += rt[x];
+						dd[fa]--;
+						if (dd[fa] == 0) {
+							q.push(fa);
+						}
+					}
+				}
+			}
+		} // namespace sam
+
 	} // namespace str
 } // namespace acm
 
