40845ea41631b9648a7416f4448996a58269dce5
Zhuanhao Wu <allen_ng@foxmail.com>
2016-05-19 03:31:27 -0700

    Add number theory utilities(miller rabin, pollard rho)

--- utils.h
+++ utils.h
@@ -2,36 +2,47 @@
 #define _ACM_UTILS
 namespace acm {
 	namespace utils {
+				typedef long long ll;
+		using namespace std;
+
+		ll add(ll a, ll b, ll mod) {
+			ll res = a + b;
+			while(res < 0) res += mod;
+			while(res >= mod) res -= mod;
+			return res;
+		}
+		// untested
+		// a * b % mod
+		// O(logb)
+		ll qmul(ll a, ll b, ll mod) {
+			a %= mod;
+			b %= mod;
+			ll ans = 0;
+			while(b) {
+				if(b & 1) {
+					ans = (ans + a)%mod;
+				}
+				a = (a+ a)%mod;
+				b >>= 1;
+			}
+			return ans;
+		}
 		// untested
 		// a^n % mod
 		// O(logn)
-		typedef long long ll;
-		using namespace std;
-
 		ll qpow(ll a, ll n, ll mod) {
+			a %= mod;
+			n %= mod;
 			ll ans = 1LL;
 			while(n) {
-				if(n & 1) ans = ans * a % mod;
-				a = (a * a) % mod;
+				if(n & 1) ans = (ans* a% mod);
+				a = (a* a% mod);
 				n >>= 1;
 			}
 			return ans;
 		}
 
-		// untested
-		// a * b % mod
-		// O(logb)
-		ll qmul(ll a, ll b, ll mod) {
-			ll ans = 0;
-			while(b) {
-				if(b & 1) {
-					ans = (ans + a) % mod;
-				}
-				a = (a + a) % mod;
-				b >>= 1;
-			}
-			return ans;
-		}
+		
 
 		// untested
 		ll gcd(ll a, ll b) {
@@ -89,7 +100,99 @@ namespace acm {
 			}
 			return a == 0 ? lcm[N - 1] : a;
 		}
+		// ******************************************
+		// Miller Rabin 素性检测和 pollard rho分解整数
+		// ******************************************
+		bool randomized = false;
+		// n - 1 = x * 2 ^ t,  a^(n - 1) = a^(x*2^t) = 1 mod (n)
+		bool is_composite(ll a, ll n, ll x, ll t) {
+			ll ret = qpow(a, x, n);
+			ll last = ret;
+			for(int i = 1; i <= t; i++) {
+				ret = qmul(ret, ret, n);
+				// 合数有非平凡根(二次同余的解)
+				if(ret == 1 && last != 1 && last != n - 1) return true;
+				last = ret;
+			}
+			if(ret != 1) return true;
+			return false;
+		}
 
+		// S 测试次数
+		bool mlrb(ll n, int S = 20) {
+			if(!randomized) { srand(time(NULL)); randomized = true; }
+			if(n < 2) return false;
+			if(n == 2) return true;
+			if((n&1) == 0) return false; // even
+			ll x = n - 1, t = 0;
+			while((x&1) == 0) { x >>= 1; t++; }
+
+			for(int i = 0; i < S; i++) {
+				ll a = rand() % (n - 1) + 1;
+				if(is_composite(a, n, x, t)) return false;
+			}
+			return true;
+		}
+
+		// ***********
+		// pollard_rho
+		// ***********
+		// 分解后无序, 如有必要需要排序!!!!
+		ll factor[100];
+		int tot;
+		ll pollard_rho(ll n) {
+			if(!randomized) { srand(time(0)); randomized = true; }
+			ll i = 1, k = 2;
+			ll x0 = rand() % n;
+			ll y = x0;
+			ll c = (rand() % (n - 1)) + 1;
+			while(1) {
+				i++;
+				x0 = qmul(x0, x0, n);
+				ll d = gcd(y - x0, n);
+				if(d != 1 && d != n) return d;
+				if(y == x0) return n;
+				if(i == k) {
+					y = x0;
+					k += k;
+				}
+			}
+		}
+
+		void find_factor(ll n) {
+			if(mlrb(n)) {
+				factor[tot++] = n;
+				return;
+			}
+			ll p = n;
+			while(p >= n) p = pollard_rho(p);
+			find_factor(p);
+			find_factor(n / p);
+		}
+
+		bool vis[maxn + 10];
+		int prime[maxn + 10];
+		int mu[maxn + 10], cnt = 0;
+		void sieve() {
+			memset(vis, 0, sizeof vis);
+			mu[1] = 1;
+			rep(i, 2, maxn) {
+				if(!vis[i]) {
+					prime[cnt++] = i;
+					mu[i] = -1;
+				}
+				rep(j, 0, cnt) {
+					if(ll(i) * prime[j] > maxn) break;
+					vis[i * prime[j]] = true;
+					if(i % prime[j] == 0) {
+						mu[i * prime[j]] = 0;
+						break;
+					} else { // i is prime
+					mu[i * prime[j]] = -mu[i];
+					}
+				}
+			}
+		}
 
 	}
 }
