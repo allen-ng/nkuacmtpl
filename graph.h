37df7ee72845ccd588a98aca4c949f3ea330b2a3
Allen Ng <allen_ng@foxmail.com>
2016-10-29 09:31:25 +0800

    Fix graph bugs, add maxflow

--- graph.h
+++ graph.h
@@ -22,7 +22,7 @@ namespace acm {
 		queue<int> q;
 
 		void graphinit() {
-			memset(head, -1, sizeof(head));
+			rep(i, 0, N) head[i] = -1;
 			sz = 0;
 		}
 		void addedge(int u, int v, int cp, int ct) {
@@ -65,6 +65,71 @@ namespace acm {
 			}
 			return make_pair(mf, mc);
 		}
+		struct MaximumFlow {
+#define each(it,o) for(auto it = (o).begin(); it != (o).end(); ++ it)
+			static const int INF = 0x3f3f3f3f;
+			typedef int Index;
+			typedef int Flow;
+			static const Flow InfCapacity = INF;
+			struct Edge {
+				Index to;
+				Flow capacity;
+				Index rev;
+			};
+			vector<vector<Edge> > g;
+			void init(Index n) { g.assign(n, vector<Edge>()); }
+			void add(Index i, Index j, Flow capacity) {
+				Edge e, f; e.to = j, f.to = i; e.capacity = capacity, f.capacity = 0;
+				g[i].push_back(e); g[j].push_back(f);
+				g[i].back().rev = (Index)g[j].size() - 1; g[j].back().rev = (Index)g[i].size() - 1;
+			}
+			void addB(Index i, Index j, Flow capacity) {
+				Edge e, f; e.to = j, f.to = i; e.capacity = capacity, f.capacity = capacity;
+				g[i].push_back(e); g[j].push_back(f);
+				g[i].back().rev = (Index)g[j].size() - 1; g[j].back().rev = (Index)g[i].size() - 1;
+			}
+			//gを破壊する
+			Flow maximumFlow(int s, int t) {
+				int n = g.size();
+				vector<Index> level(n);
+				Flow total = 0; bool update;
+				do {
+					update = false;
+					fill(level.begin(), level.end(), -1); level[s] = 0;
+					queue<Index> q; q.push(s);
+					for(Index d = n; !q.empty() && level[q.front()] < d; ) {
+						int u = q.front(); q.pop();
+						if(u == t) d = level[u];
+						each(e, g[u]) if(e->capacity > 0 && level[e->to] == -1)
+							q.push(e->to), level[e->to] = level[u] + 1;
+					}
+					vector<Index> iter(n);
+					for(Index i = 0; i < n; i ++) iter[i] = (int)g[i].size() - 1;
+					while(1) {
+						Flow f = augment(level, iter, s, t, InfCapacity);
+						if(f == 0) break;
+						total += f; update = true;
+					}
+				} while(update);
+				return total;
+			}
+			Flow augment(vector<Index> &level, vector<Index> &iter, Index u, Index t, Flow f) {
+				if(u == t || f == 0) return f;
+				Index lv = level[u];
+				if(lv == -1) return 0;
+				level[u] = -1;
+				for(; iter[u] >= 0; -- iter[u]) {
+					Edge &e = g[u][iter[u]];
+					if(level[e.to] <= lv) continue;
+					Flow l = augment(level, iter, e.to, t, min(f, e.capacity));
+					if(l == 0) continue;
+					e.capacity -= l; g[e.to][e.rev].capacity += l;
+					level[u] = lv;
+					return l;
+				}
+				return 0;
+			}
+		};
 
 	} // graph
 }// acm
